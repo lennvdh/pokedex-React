2ee9b2babdf3af0b52964c7078bf5f415dc0b99d
lennvdh <lenn.vanderheijden@gmail.com>
2020-10-02 16:22:22 +0200

    installing project

---
+++ pokedex/src/components/FetchingData.js
@@ -0,0 +1,11 @@
+import React, {useState} from "react";
+
+export default function FetchingData() {
+    let [responseData, setResponseData] = useState('');
+
+    return(
+        <div>
+
+        </div>
+    )
+}
