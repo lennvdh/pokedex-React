2f72ead529650fd7854cd2fe3de16c5eface1afd
lennvdh <lenn.vanderheijden@gmail.com>
2020-10-05 16:22:43 +0200

    fetching data in react axios

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import './App.css';
+import FetchingData from "./components/FetchingData";
 
 function App() {
 
   return (
     <div className="App">
-
+      <FetchingData/>
     </div>
   );
 }
--- pokedex/src/components/FetchingData.js
+++ pokedex/src/components/FetchingData.js
@@ -1,11 +1,59 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
+import axios from 'axios';
 
-export default function FetchingData() {
-    let [responseData, setResponseData] = useState('');
+
+export default function FetchingData(props) {
+    // hooks
+    const [pokemons, setPokemons] = useState([]);
+    const [pokename, setPokename] = useState('');
+
+    useEffect(()=>{
+        let url = `https://pokeapi.co/api/v2/pokemon/${pokename}`;
+        axios.get(url)
+            .then(res => {
+                //console.log(res.data.name)
+                setPokemons(res.data)
+                //props.onSubmit(res.data);
+                setPokename('');
+            })
+
+            .catch(err=>{
+                console.log(err)
+            })
+    })
+    /*useEffect(()=>{
+        let urlID = `https://pokeapi.co/api/v2/pokemon/${pokeID}`
+        axios.get(urlID)
+            .then(resp =>{
+                //console.log(resp.data)
+                //props.onSubmit(resp.data)
+                setPokeID('');
+            })
+            .catch(err => console.log(err))
+    })*/
 
     return(
         <div>
-
+            <ul>
+                <input value={pokename}
+                    onChange={event => setPokename(event.target.value)}
+                    placeholder="pokemon name"
+                />
+                <div className="drop-down">
+                    <select>
+                        {
+                            pokemons.map((pokemon => <option value={pokemon.name}>{pokemon.name}</option> ))
+                        }
+                    </select>
+                </div>
+                <h1>{pokemons.name}</h1>
+                {/*{pokemons.map((pokemon => <li key={pokemon.data.results.id}>{pokemon.data.results.name}</li>))}*/}
+                {/*{pokemons.forEach(pokemon => <li key={pokemon.data.results.id}>{pokemon.data.results.name}</li> )}*/}
+            </ul>
         </div>
     )
+
+
+
+
 }
